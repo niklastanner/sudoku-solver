bc8f4ef9ad0866bd75b94bd19eb0ae5d2417ffa6
Niklas Tanner <niklas.tanner@hotmail.com>
2021-01-01 14:51:55 +0100

    Implement Sudoku matrix

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Sudoku_Solver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int?[] init = new int?[81];
+            int?[,] game = new int?[81, 11];
+
+            init[4] = 8; init[10] = 1; init[11] = 7; init[15] = 3; init[16] = 2; init[20] = 3; init[21] = 7; init[22] = 2; init[23] = 6; init[24] = 1;
+            init[27] = 2; init[28] = 5; init[31] = 6; init[34] = 1; init[35] = 8; init[45] = 6; init[46] = 3; init[49] = 5; init[52] = 4; init[53] = 7;
+            init[56] = 8; init[57] = 5; init[58] = 4; init[59] = 1; init[60] = 6; init[64] = 7; init[65] = 2; init[69] = 4; init[70] = 9; init[76] = 7;
+
+            for (int i = 0; i < init.Length; i++)
+            {
+                game[i, 0] = init[i];
+            }
+
+            Sudoku sudoku = new Sudoku(game);
+            Console.WriteLine("Hello World");
         }
     }
 }
---
+++ Sudoku.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku_Solver
+{
+    class Sudoku
+    {
+        private const int SIZE = 81;
+
+        private int?[,] game = new int?[SIZE,11];
+
+        public Sudoku(int?[,] init)
+        {
+            game = init;
+
+            for(int i = 0; i < SIZE; i++)
+            {
+                if(game[i,0] == null)
+                {
+                    for(int j = 1; j < 11; j++)
+                    {
+                        game[i, j] = j-1;
+                    }
+                }
+            }
+        }
+
+        public int? Get (int index)
+        {
+            return game[index, 0];
+        }
+
+        public void Set (int index, int? value)
+        {
+            game[index, 0] = value;
+        }
+
+        public void RemovePossibility (int index, int value)
+        {
+            game[index, value] = null;
+        }
+    }
+}
