7cf4a4633712ab9d61631419edd62e1c6c85085d
Niklas Tanner <niklas.tanner@hotmail.com>
2021-01-08 13:47:07 +0100

    Add factory method using user input

---
+++ Factory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Sudoku_Solver
+{
+    static class Factory
+    {
+        public static Sudoku Create()
+        {
+            Console.WriteLine("Reading sudoku from user input");
+            int[] init = new int[81];
+            for (int i = 0; i <= 72; i += 9)
+            {
+                Console.Write("Line {0}: ", i / 9 + 1);
+                string input = Console.ReadLine();
+                char[] line = input.ToCharArray();
+                try
+                {
+                    if (input.Length == 9)
+                    {
+                        for (int j = 0; j < line.Length; j++)
+                        {
+                            init[i + j] = int.Parse(line[j].ToString());
+                        }
+                    }
+                    else
+                    {
+                        throw new IndexOutOfRangeException("Input does not contain 9 characters");
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (e is IndexOutOfRangeException || e is FormatException)
+                    {
+                        Console.WriteLine(e.Message);
+                        i -= 9;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            Console.WriteLine("");
+
+            return new Sudoku(init);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Sudoku_Solver
+﻿namespace Sudoku_Solver
 {
     class Program
     {
@@ -20,17 +18,20 @@ namespace Sudoku_Solver
             init[54] = 6; init[62] = 9; init[65] = 1; init[67] = 6; init[69] = 2; init[73] = 2; init[75] = 7; init[77] = 4; init[79] = 6;*/
 
             // Example for a medium hard sudoku
-            init[0] = 7; init[1] = 4; init[7] = 2; init[8] = 9; init[12] = 2; init[14] = 5; init[19] = 5; init[21] = 6; init[23] = 7;
-            init[25] = 8; init[27] = 5; init[35] = 1; init[37] = 6; init[43] = 9; init[45] = 9; init[53] = 5; init[55] = 7; init[57] = 9;
-            init[59] = 8; init[61] = 5; init[66] = 3; init[68] = 1; init[72] = 8; init[73] = 9; init[79] = 1; init[80] = 3;
+            /*            init[0] = 7; init[1] = 4; init[7] = 2; init[8] = 9; init[12] = 2; init[14] = 5; init[19] = 5; init[21] = 6; init[23] = 7;
+                        init[25] = 8; init[27] = 5; init[35] = 1; init[37] = 6; init[43] = 9; init[45] = 9; init[53] = 5; init[55] = 7; init[57] = 9;
+                        init[59] = 8; init[61] = 5; init[66] = 3; init[68] = 1; init[72] = 8; init[73] = 9; init[79] = 1; init[80] = 3;*/
 
             // Example for a hard sudoku
             /*            init[2] = 2; init[3] = 6; init[5] = 9; init[11] = 7; init[12] = 2; init[17] = 8; init[22] = 1; init[25] = 4;
                         init[27] = 4; init[34] = 1; init[36] = 1; init[39] = 3; init[40] = 6; init[41] = 5; init[44] = 4; init[46] = 2; init[53] = 3;
                         init[55] = 5; init[58] = 3; init[63] = 6; init[68] = 7; init[69] = 9; init[75] = 8; init[77] = 6; init[78] = 5;*/
 
+            init[3] = 7; init[4] = 1; init[7] = 8; init[8] = 3; init[9] = 2; init[15] = 4; init[21] = 4; init[24] = 9; init[25] = 7;
+            init[28] = 8; init[32] = 5; init[35] = 2; init[40] = 2; init[45] = 5; init[48] = 8; init[52] = 6;
+            init[55] = 3; init[56] = 2; init[59] = 7; init[65] = 8; init[71] = 9; init[72] = 6; init[73] = 7; init[76] = 3; init[77] = 4;
 
-            Sudoku sudoku = new Sudoku(init);
+            Sudoku sudoku = Factory.Create();
 
             Solver solver = new Solver(sudoku);
             solver.Solve();
